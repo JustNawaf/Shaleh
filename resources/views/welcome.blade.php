533d3ac9f5a249d4e966011a52d3b61876eb929a
JustNawaf <38926166+JustNawaf@users.noreply.github.com>
2019-08-01 01:04:40 +0300

    Setup Nav Bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="text-center">الصفحة الرئيسية</h1>
+<div class="container"  style="margin-top: 75%;">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Title</h5>
+            <p class="card-text">Content</p>
+        </div>
+    </div>
+    <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+    </div>
+    <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+    </div>
+    <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+    </div>
+    <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+    </div>
 </div>
 @endsection
