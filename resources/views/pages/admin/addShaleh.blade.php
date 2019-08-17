fccc1fb3d89f5beb3022deb1681ec8403e37a7c7
Nawaf <n-f555@hotmail.com>
2019-08-17 08:41:57 +0300

    Update

--- resources/views/pages/admin/addShaleh.blade.php
+++ resources/views/pages/admin/addShaleh.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5 text-right">
-    <h1 class="text-center">معلومات الشاليه</h1>
+<div class="container mt-5 text-right" style="color:white;">
+    <h1 class="text-center">اضافة الشاليه</h1>
     <div class="form-group pt-3">
         <label for="inputName">اسم الشاليه</label>
         <input type="text" class="form-control is-valid" name="inputName" id="inputName">
@@ -66,9 +66,8 @@
             <label class="custom-control-label" for="customCheck1">غرفة نوم اضافية</label>
         </div>
     </div>
-
     <div class="form-group mt-5">
-        <h1 class="text-center">الأسعار</h1>
+        <h1 class="text-center">معلومات شخصية</h1>
         <div class="row pt-3">
             <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
                 <label for="inputName">السعر في اليوم العادي</label>
@@ -100,8 +99,74 @@
                 </div>
             </div>
         </div>
+        <div class="form-group mt-5">
+            <h1 class="text-center">معلومات الموقع</h1>
+            <div class="row pt-3">
+                <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                    <label for="inputName">السعر في اليوم العادي</label>
+                    <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                    <div class="invalid-feedback" style="">
+                        Validation message
+                    </div>
+                </div>
+                <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                    <label for="inputName">السعر في رمضان</label>
+                    <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                    <div class="invalid-feedback" style="">
+                        Validation message
+                    </div>
+                </div>
 
-        <button type="submit" class="btn btn-primary w-100 mt-5 bg-dark">انشاء شاليه</button>
-    </div>
-</div>
-@endsection
+                <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                    <label for="inputName">السعر في عيد الفطر</label>
+                    <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                    <div class="invalid-feedback" style="">
+                        Validation message
+                    </div>
+                </div>
+                <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                    <label for="inputName">السعر في عيد الاضحى</label>
+                    <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                    <div class="invalid-feedback" style="">
+                        Validation message
+                    </div>
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <h1 class="text-center">الأسعار</h1>
+                <div class="row pt-3">
+                    <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                        <label for="inputName">السعر في اليوم العادي</label>
+                        <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                        <div class="invalid-feedback" style="">
+                            Validation message
+                        </div>
+                    </div>
+                    <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                        <label for="inputName">السعر في رمضان</label>
+                        <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                        <div class="invalid-feedback" style="">
+                            Validation message
+                        </div>
+                    </div>
+
+                    <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                        <label for="inputName">السعر في عيد الفطر</label>
+                        <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                        <div class="invalid-feedback" style="">
+                            Validation message
+                        </div>
+                    </div>
+                    <div class="col-12 w-100 col-md-3 col-sm-12 col-xs-4">
+                        <label for="inputName">السعر في عيد الاضحى</label>
+                        <input type="text" class="form-control is-valid" name="inputName" id="inputName">
+                        <div class="invalid-feedback" style="">
+                            Validation message
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary w-100 mt-5 bg-dark">اضافة شاليه</button>
+            </div>
+        </div>
+        @endsection
